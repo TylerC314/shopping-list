04844bb274c97029f920de9558b10f5e3a2352d9
Tyler <tylerclark314@gmail.com>
2019-05-01 01:22:18 -0700

    Finished functionality

--- index.js
+++ index.js
@@ -3,18 +3,33 @@ $("#js-shopping-list-form").submit(event => {
     event.preventDefault();
     item = $("#shopping-list-entry").val();
     if(item !== "") {
-        $(".shopping-list").append(addItem(item));
-    }    
+        $(".shopping-list").append(createItem(item));
+    }
+    $("#shopping-list-entry").val("");    
 });
 
-$(".shopping-item-toggle").click( function(event) {
-    let itemName = $(this).closest("li").find(".shopping-item").addClass("shopping-item__checked");
+//When check button is pressed on item, crosses out item name
+$(".shopping-list").on("click", ".shopping-item-toggle", function(event) {
+    $(this).closest("li").find(".shopping-item").addClass("shopping-item__checked");
 });
 
-//Creates the default list item.
-function addItem(name) {
-    let defaultItem = $(".shopping-list").find("li").first().clone();
-    defaultItem.find(".shopping-item").text(name);
-    item = defaultItem.html();
-    return "<li>" + item + "</li>";
+//When delete button is pressed on item. item is deleted
+$(".shopping-list").on("click", ".shopping-item-delete", function(event) {
+    $(this).closest("li").remove();
+});
+
+//generates html for an item with the parameter as its name.
+function createItem(name) {
+    let item = "<li>" +
+    "<span class='shopping-item'>" + name + "</span>" +
+    "<div class='shopping-item-controls'>" +
+      "<button class='shopping-item-toggle'>" +
+        "<span class='button-label'>check</span>" +
+      "</button>" +
+      "<button class='shopping-item-delete'>" +
+        "<span class='button-label'>delete</span>" +
+      "</button>" +
+    "</div>" +
+  "</li>";
+  return item;
 }
